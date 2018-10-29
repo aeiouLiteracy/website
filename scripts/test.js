136e2ed1e04a7f1e4ceb765220b148ad0fa49cb6
Andrew Patrick Ong Tolentino <atolentino@hawk.iit.edu>
2018-10-29 18:18:52 -0500

    Dots now change colors corresponding to current picture on slide show. But the hover still does not work over the dots

--- scripts/test.js
+++ scripts/test.js
@@ -1,19 +1,32 @@
 var k = 0;
 var slides = document.getElementsByClassName("slideShowElement");
+var dots = document.getElementsByClassName("dot");
 slideShow();
+var interval = setInterval(slideShow, 4000);
 
 function slideShow() {
-  console.log(k);
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+    dots[i].style.backgroundColor = "#bbb";
+
   }
   if (k > slides.length - 1) {
     k = 0;
+    console.log(k);
     slides[k].style.display = "block";
-  }
-  else {
-    slides[k].style.display = "block";
+    dots[k].style.backgroundColor = "#717171";
     k++;
   }
-  setTimeout(slideShow, 4000);
+  else {
+    console.log(k);
+    slides[k].style.display = "block";
+    dots[k].style.backgroundColor = "#717171";
+    k++;
+  }
+}
+function changeSlideIndex(x) {
+  clearInterval(interval);
+  k = x;
+  slideShow();
+  interval = setInterval(slideShow, 4000);
 }
