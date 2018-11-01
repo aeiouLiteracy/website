dbea4bfe5052e737bc6aefb1e1927837d1483d77
Andrew Patrick Ong Tolentino <atolentino@hawk.iit.edu>
2018-11-01 18:50:02 -0500

    Add drop down menu on Media but it is not styled well. Also added deleted pictures from my local repo.

--- scripts/test.js
+++ scripts/test.js
@@ -24,9 +24,26 @@ function slideShow() {
     k++;
   }
 }
+
 function changeSlideIndex(x) {
   clearInterval(interval);
   k = x;
   slideShow();
   interval = setInterval(slideShow, 4000);
+}
+
+function dropDownToggle() {
+  document.getElementById("dropDownLinks").classList.toggle("show");
+}
+
+window.onClick = function(event) {
+  if (!event.target.matches('.figureButton')) {
+    var dropDownContent = document.getElementsByClassName('dropDownContent');
+    for (let i = 0; i < dropDownContent.length; i++) {
+      let temp = dropDownContent[i];
+      if (temp.classList.contains('show')) {
+        temp.classList.remove('show');
+      }
+    }
+  }
 }
